5b2f3c259cd0b9d6a2882c89c05aad532cc76d94
Arif <dopymonster@gmail.com>
2026-08-10 21:55:35 +0800

    update ui login and register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,80 +2,82 @@
 <html lang="en">
 
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-	<link rel="stylesheet" href="{{ URL::asset('assets/css/profile/login.css') }}">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TalentBank</title>
+    <link rel="shortcut icon" href="{{ URL::asset('assets/images/logo/JobTalent-logo-cropped-cropped.svg') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link href="{{ URL::asset('assets/libs/bootstrap5/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/profile/login.css') }}">
 </head>
 
 <body>
-	<div class="card-container">
-		<div class="card-content p-4">
-			<div class="d-flex justify-content-center">
-				<img class="w-50" src="{{ URL::asset('assets/images/profile/newLogo.png') }}" alt="">
-			</div>
-			<h1 class="text-center">Welcome Back</h1>
-			<p class="text-center">Sign in to continue to TalentBank</p>
+    <div class="card-container">
+        <div class="card-content p-4">
+            <div class="d-flex justify-content-center">
+                <img class="w-50" src="{{ URL::asset('assets/images/profile/newLogo.png') }}" alt="">
+            </div>
+            <h1 class="text-center">Welcome Back</h1>
+            <p class="text-center">Sign in to continue to TalentBank</p>
 
-			<form action="{{ route('login') }}" method="POST">
-				@csrf
+            <form action="{{ route('login') }}" method="POST">
+                @csrf
 
-				<div class="mb-3">
-					<label for="email" class="form-label">Email Address</label>
-					<div class="input-group has-validation">
-						<span class="input-group-text bg-white" id="basic-addon-email">
-							<i class="fa-regular fa-envelope"></i>
-						</span>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email Address</label>
+                    <div class="input-group has-validation">
+                        <span class="input-group-text bg-white" id="basic-addon-email">
+                            <i class="fa-regular fa-envelope"></i>
+                        </span>
 
-						<input type="email" name="email" id="email"
-							class="form-control @error('email') is-invalid @enderror"
-							aria-label="Email" aria-describedby="basic-addon-email">
+                        <input type="email" name="email" id="email"
+                            class="form-control @error('email') is-invalid @enderror" aria-label="Email"
+                            aria-describedby="basic-addon-email">
 
-						@error('email')
-						<div class="invalid-feedback">
-							{{ $message }}
-						</div>
-						@enderror
-					</div>
-				</div>
+                        @error('email')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                </div>
 
-				<div class="mb-3">
-					<label for="password" class="form-label">Password</label>
-					<div class="input-group has-validation">
-						<span class="input-group-text bg-white" id="basic-addon-password">
-							<i class="fas fa-lock"></i>
-						</span>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <div class="input-group has-validation">
+                        <span class="input-group-text bg-white" id="basic-addon-password">
+                            <i class="fas fa-lock"></i>
+                        </span>
 
-						<input type="password" name="password" id="password"
-							class="form-control @error('password') is-invalid @enderror" aria-label="Password"
-							aria-describedby="basic-addon-password">
+                        <input type="password" name="password" id="password"
+                            class="form-control @error('password') is-invalid @enderror" aria-label="Password"
+                            aria-describedby="basic-addon-password">
 
-						@error('password')
-						<div class="invalid-feedback">
-							{{ $message }}
-						</div>
-						@enderror
-					</div>
-				</div>
+                        @error('password')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                </div>
 
-				<div class="d-flex justify-content-between my-2">
-					<div></div>
-					<a href="" class="text-primary fw-bold text-decoration-none">Forgot password?</a>
-				</div>
+                <div class="d-flex justify-content-between my-2">
+                    <div></div>
+                    <a href="" class="text-primary fw-bold text-decoration-none">Forgot password?</a>
+                </div>
 
-				<button type="submit" class="btn btn-primary w-100">Sign In</button>
+                <button type="submit" class="btn btn-primary w-100">Sign In</button>
 
-			</form>
+            </form>
 
-			<div class="d-flex mt-4 justify-content-center">
-				<p class="mb-0 me-1">Don't have an account? - </p>
-				<a href="{{ route('registerPage') }}" class="text-primary fw-bold text-decoration-none">Create account</a>
-			</div>
-		</div>
-	</div>
+            <div class="d-flex mt-4 justify-content-center">
+                <p class="mb-0 me-1">Don't have an account? - </p>
+                <a href="{{ route('registerPage') }}" class="text-primary fw-bold text-decoration-none">Create
+                    account</a>
+            </div>
+        </div>
+    </div>
+    <script src="{{ URL::asset('assets/libs/bootstrap5/bootstrap.min.js') }}"></script>
 </body>
 
 </html>
-
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,10 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>TalentBank</title>
+    <link rel="shortcut icon" href="{{ URL::asset('assets/images/logo/JobTalent-logo-cropped-cropped.svg') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link href="{{ URL::asset('assets/libs/bootstrap5/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="{{ URL::asset('assets/css/profile/login.css') }}">
 </head>
 
@@ -15,7 +15,8 @@
     <div class="card-container">
         <div class="card-content p-4">
             <div class="d-flex justify-content-center">
-                <img class="w-50" src="./newLogo.png" alt="">
+                <img class="w-50" src="{{ URL::asset('assets/images/profile/newLogo.png') }}" alt="">
+
             </div>
             <h1 class="text-center">Create Your Account</h1>
             <p class="text-center">Join TalentBank and build your professional future</p>
@@ -29,66 +30,67 @@
                         <span class="input-group-text bg-white" id="basic-addon-name">
                             <i class="fa-solid fa-user"></i>
                         </span>
-                            <input type="text" name="name" id="name"
-                                class="form-control @error('name') is-invalid @enderror" value="{{ old('name') }}"
-                                aria-label="Full Name" aria-describedby="basic-addon-name">
+                        <input type="text" name="name" id="name"
+                            class="form-control @error('name') is-invalid @enderror" value="{{ old('name') }}"
+                            aria-label="Full Name" aria-describedby="basic-addon-name">
 
-                            @error('name')
-                                <div class="invalid-feedback">
-                                    {{ $message }}
-                                </div>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="email" class="form-label">Email Address</label>
-                        <div class="input-group has-validation">
-                            <span class="input-group-text bg-white" id="basic-addon-email">
-                                <i class="fa-regular fa-envelope"></i>
-                            </span>
-
-                            <input type="email" name="email" id="email"
-                                    class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}"
-                                    aria-label="Email" aria-describedby="basic-addon-email">
-
-                                @error('email')
-                                    <div class="invalid-feedback">
-                                        {{ $message }}
-                                    </div>
-                                @enderror
+                        @error('name')
+                            <div class="invalid-feedback">
+                                {{ $message }}
                             </div>
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="password" class="form-label">Password</label>
-                            <div class="input-group has-validation">
-                                <span class="input-group-text bg-white" id="basic-addon-password">
-                                    <i class="fa-solid fa-lock"></i>
-                                </span>
-
-                                    <input type="password" name="password" id="password"
-                                        class="form-control @error('password') is-invalid @enderror" aria-label="Password"
-                                        aria-describedby="basic-addon-password">
-
-                                    @error('password')
-                                        <div class="invalid-feedback">
-                                            {{ $message }}
-                                        </div>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <button type="submit" class="btn btn-primary w-100">Create Account</button>
-
-                        </form>
-
-                        <div class="d-flex mt-4 justify-content-center">
-                            <p class="mb-0 me-1">Already have an account? - </p>
-                            <a href="{{ route('loginPage') }}" class="text-primary fw-bold text-decoration-none">Sign in</a>
-                        </div>
+                        @enderror
                     </div>
                 </div>
-            </body>
 
-            </html>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email Address</label>
+                    <div class="input-group has-validation">
+                        <span class="input-group-text bg-white" id="basic-addon-email">
+                            <i class="fa-regular fa-envelope"></i>
+                        </span>
+
+                        <input type="email" name="email" id="email"
+                            class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}"
+                            aria-label="Email" aria-describedby="basic-addon-email">
+
+                        @error('email')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <div class="input-group has-validation">
+                        <span class="input-group-text bg-white" id="basic-addon-password">
+                            <i class="fa-solid fa-lock"></i>
+                        </span>
+
+                        <input type="password" name="password" id="password"
+                            class="form-control @error('password') is-invalid @enderror" aria-label="Password"
+                            aria-describedby="basic-addon-password">
+
+                        @error('password')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                </div>
+
+                <button type="submit" class="btn btn-primary w-100">Create Account</button>
+
+            </form>
+
+            <div class="d-flex mt-4 justify-content-center">
+                <p class="mb-0 me-1">Already have an account? - </p>
+                <a href="{{ route('loginPage') }}" class="text-primary fw-bold text-decoration-none">Sign in</a>
+            </div>
+        </div>
+    </div>
+    <script src="{{ URL::asset('assets/libs/bootstrap5/bootstrap.min.js') }}"></script>
+</body>
+
+</html>
