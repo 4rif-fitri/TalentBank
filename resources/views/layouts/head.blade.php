d2470b2b91986f9a1f0a4446f145b47f5360048a
Arif <dopymonster@gmail.com>
2026-08-12 23:59:46 +0800

    update layout css

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,8 +1,8 @@
 <!-- App css -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-<link rel="stylesheet" href="{{ asset('assets/internship-assets/libs/bootstrap.min.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/internship-assets/libs/jquery-ui.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/internship-assets/layout/newLayout.css') }}">
-<link rel="stylesheet" href="{{asset('assets/internship-assets/style/style.css')}}">
+<link rel="stylesheet" href="{{ URL::asset('assets/internship-assets/libs/jquery-ui.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('assets/internship-assets/libs/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('assets/internship-assets/layout/newLayout.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('assets/internship-assets/style/style.css')}}">
 <!-- App css -->
 @yield('css')
--- resources/views/layouts/internship-layouts.blade.php
+++ resources/views/layouts/internship-layouts.blade.php
@@ -25,19 +25,7 @@
         @yield('content')
     </main>
 
-    <script src="{{asset('assets/internship-assets/libs/jquery-4.0.0.min.js')}}"></script>
-    <script src="{{asset('assets/internship-assets/libs/jquery-ui.min.js')}}"></script>
-    <script src="{{asset('assets/internship-assets/libs/bootstrap.min.js')}}"></script>
-    <script src="{{asset('assets/internship-assets/libs/sweetalert2@11.js')}}"></script>
-    <script src="{{asset('assets/internship-assets/layout/newLayout.js')}}"></script>
-
-    <script>
-        if (window.innerWidth > 768 && localStorage.getItem("sidebarCollapsed") === "true") {
-            document.documentElement.classList.add("sidebar-collapsed-init");
-        }
-    </script>
-    @yield('script')
-
+    @include('layouts.footer-script')
 </body>
 
 </html>
