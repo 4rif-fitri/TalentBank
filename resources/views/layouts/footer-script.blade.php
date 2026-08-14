0b73bdde14344c540883d8ed3a81958fe009bb7a
Arif <dopymonster@gmail.com>
2026-08-15 00:21:43 +0800

    try add scss

--- resources/views/layouts/footer-script.blade.php
+++ resources/views/layouts/footer-script.blade.php
@@ -1,7 +1,6 @@
 <!-- JAVASCRIPT -->
 <script src="{{URL::asset('assets/internship-assets/libs/jquery-4.0.0.min.js')}}"></script>
 <script src="{{URL::asset('assets/internship-assets/libs/jquery-ui.min.js')}}"></script>
-<script src="{{URL::asset('assets/internship-assets/libs/bootstrap.min.js')}}"></script>
 <script src="{{URL::asset('assets/internship-assets/libs/sweetalert2@11.js')}}"></script>
 <script src="{{URL::asset('assets/internship-assets/layout/newLayout.js')}}"></script>
 <script>
--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,8 +1,13 @@
-<!-- App css -->
+<!-- Vendor CSS -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 <link rel="stylesheet" href="{{ URL::asset('assets/internship-assets/libs/jquery-ui.css') }}">
-<link rel="stylesheet" href="{{ URL::asset('assets/internship-assets/libs/bootstrap.css') }}">
+<!-- Bootstrap via Vite -->
+@vite([
+    'resources/scss/app.scss',
+    'resources/js/app.js'
+])
+<!-- TalentBank Custom CSS -->
 <link rel="stylesheet" href="{{ URL::asset('assets/internship-assets/layout/newLayout.css') }}">
-<link rel="stylesheet" href="{{ URL::asset('assets/internship-assets/style/style.css')}}">
-<!-- App css -->
+<link rel="stylesheet" href="{{ URL::asset('assets/internship-assets/style/style.css') }}">
+
 @yield('css')
