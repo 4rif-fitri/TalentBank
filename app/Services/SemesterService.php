dd0f551e27910e6a58dc39a9bddb5249ebf402c5
EugeneHZC <eugenehzc06@gmail.com>
2026-08-18 16:08:28 +0800

    Implemented code to upload images for update education

--- app/Services/SemesterService.php
+++ app/Services/SemesterService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Models\Media;
+use App\Models\Semester;
 use Exception;
 use Illuminate\Http\Response;
 use Illuminate\Http\UploadedFile;
@@ -44,5 +45,56 @@ class SemesterService
         $data['file'] = $file;
 
         return $this->mediaService->createMedia($data, $userProfileId);
+    }
+
+    /**
+     * Creates a new semester record
+     * 
+     * @param array $data
+     * @throws Exception
+     * @return Semester
+     */
+    public function createSemester(array $data): Semester
+    {
+        $semesterExists = Semester::where([
+            'education_id' => $data['education_id'],
+            'session' => $data['session']
+        ])->exists();
+
+        if ($semesterExists) {
+            throw new Exception('Given session for this semester already exists.', Response::HTTP_CONFLICT);
+        }
+
+        $semester = Semester::create([
+            'education_id' => $data['education_id'],
+            'gpa' => $data['gpa'],
+            'session' => $data['session']
+        ]);
+
+        return $semester;
+    }
+
+    /**
+     * Updates existing semester based on semester ID and user profile ID
+     * 
+     * @param array $data
+     * @param int $semesterId
+     * @param int $userProfileId
+     * @return Semester
+     */
+    public function updateSemester(array $data, int $semesterId, int $userProfileId): Semester
+    {
+        $semester = Semester::where('id', $semesterId)
+            ->whereHas('education', function ($query) use ($userProfileId) {
+                $query->where('user_profile_id', $userProfileId);
+            });
+
+        $semester = Semester::create([
+            'education_id' => $data['education_id'],
+            'gpa' => $data['gpa'],
+            'session' => $data['session']
+        ]);
+
+        return $semester;
     }
 }
