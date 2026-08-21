737faf73641b2d7c00124a4c444c0ad67cb9c45a
EugeneHZC <eugenehzc06@gmail.com>
2026-08-21 10:39:04 +0800

    Implement code to create, update and delete user skills through update education api

--- app/Services/SkillService.php
+++ app/Services/SkillService.php
@@ -10,25 +10,32 @@ use Illuminate\Support\Collection;
 
 class SkillService
 {
-    private function verifySkillOwnership(UserSkill $userSkill, int $userProfileId): void
+    private function verifySkillOwnership(UserSkill|Collection $userSkills, int $userProfileId): void
     {
-        // get the user profile ID
-        $ownerProfileId = $userSkill->source_type == 'user_profile' ?
-            $userSkill?->source_id :
-            $userSkill->source?->user_profile_id;
+        $userSkills = $userSkills instanceof Collection ? $userSkills : collect([$userSkills]);
+        $userSkills->loadMissing('source');
 
-        if ($ownerProfileId != $userProfileId) {
-            throw new Exception('Unauthorized access to this skill.', Response::HTTP_FORBIDDEN);
+        // get the user profile ID
+        foreach ($userSkills as $userSkill) {
+            $ownerProfileId = $userSkill->source_type === 'user_profile' ?
+                $userSkill?->source_id :
+                $userSkill->source?->user_profile_id;
+
+            if ($ownerProfileId != $userProfileId) {
+                throw new Exception('Unauthorized access to this skill.', Response::HTTP_FORBIDDEN);
+            }
         }
     }
 
     private function getUserSkillModelById(int $userSkillId, string $sourceType, int $sourceId, int $userProfileId): UserSkill
     {
-        $userSkill = UserSkill::where([
-            'id' => $userSkillId,
-            'source_type' => $sourceType,
-            'source_id' => $sourceId
-        ])->first();
+        $userSkill = UserSkill::with('source')
+            ->where([
+                'id' => $userSkillId,
+                'source_type' => $sourceType,
+                'source_id' => $sourceId
+            ])
+            ->first();
 
         if (!isset($userSkill)) {
             throw new Exception('User skill not found with given ID.', Response::HTTP_NOT_FOUND);
@@ -40,20 +47,21 @@ class SkillService
         return $userSkill;
     }
 
-    private function checkUserSkillExists(int $skillId, string $sourceType, int $sourceId, ?int $userSkillId = null): void
+    private function checkUserSkillsExists(array|int $skillIds, string $sourceType, int $sourceId, array|int|null $userSkillIds = null): void
     {
-        $userSkillExists = UserSkill::where([
-            'source_type' => $sourceType,
-            'source_id' => $sourceId,
-            'skill_id' => $skillId,
-        ])
-            ->when(isset($userSkillId), function ($query) use ($userSkillId) {
-                $query->where('id', '<>', $userSkillId);
+        $skillIds = (array) $skillIds;
+        $userSkillIds = (array) $userSkillIds;
+
+        $userSkillExists = UserSkill::whereIn('skill_id', $skillIds)
+            ->where('source_type', $sourceType)
+            ->where('source_id', $sourceId)
+            ->when(!empty($userSkillIds), function ($query) use ($userSkillIds) {
+                $query->whereNotIn('id', $userSkillIds);
             })
             ->exists();
 
         if ($userSkillExists) {
-            throw new Exception('User skill already exist in profile.', Response::HTTP_CONFLICT);
+            throw new Exception('User skill(s) already exist in profile.', Response::HTTP_CONFLICT);
         }
     }
 
@@ -77,7 +85,7 @@ class SkillService
     public function createUserSkill(array $data): UserSkill
     {
         // check if user skill already exists
-        $this->checkUserSkillExists($data['skill_id'], $data['source_type'], $data['source_id']);
+        $this->checkUserSkillsExists($data['skill_id'], $data['source_type'], $data['source_id']);
 
         // create a new user skill
         $userSkill = UserSkill::create([
@@ -85,6 +93,39 @@ class SkillService
             'source_id' => $data['source_id'],
             'skill_id' => $data['skill_id'],
         ]);
+
+        return $userSkill;
+    }
+
+    /**
+     * Creates multiple new user skills
+     * (meant to be used by other services to create their respective skills)
+     * 
+     * @param array $data  // new user_skills to be inserted
+     * @throws Exception
+     * @return bool
+     */
+    public function createUserSkills(array $data, string $sourceType, int $sourceId): bool
+    {
+        if (empty($data)) {
+            return true;
+        }
+
+        $insertRecord = [];
+
+        // check if user skill already exists
+        $this->checkUserSkillsExists($data, $sourceType, $sourceId);
+
+        foreach ($data as $newSkillId) {
+            $insertRecord[] = [
+                'source_type' => $sourceType,
+                'source_id' => $sourceId,
+                'skill_id' => $newSkillId
+            ];
+        }
+
+        // create a new user skill
+        $userSkill = UserSkill::insert($insertRecord);
 
         return $userSkill;
     }
@@ -103,7 +144,7 @@ class SkillService
         $userSkill = $this->getUserSkillModelById($userSkillId, $data['source_type'], $data['source_id'], $userProfileId);
 
         // check if user skill already exists in profile
-        $this->checkUserSkillExists($data['skill_id'], $data['source_type'], $data['source_id'], $userSkillId);
+        $this->checkUserSkillsExists($data['skill_id'], $data['source_type'], $data['source_id'], $userSkillId);
 
         // update user skill
         $result = $userSkill->update([
@@ -111,6 +152,45 @@ class SkillService
         ]);
 
         return $result;
+    }
+
+    /**
+     * Updates multiple existing user skills
+     * (meant to be used by other services to update their respective skills)
+     * 
+     * @param array $data
+     * @param int $userProfileId
+     * @return bool|int
+     */
+    public function updateUserSkills(array $data, int $userProfileId, string $sourceType, int $sourceId): bool
+    {
+        if (empty($data)) {
+            return true;
+        }
+
+        $updateRecord = [];
+        $userSkillIds = array_column($data, 'id');
+        $skillIds = array_column($data, 'skill_id');
+
+        // validate data given
+        $userSkills = UserSkill::with('source')->whereIn('id', $userSkillIds)->get();
+        $this->verifySkillOwnership($userSkills, $userProfileId);
+        $this->checkUserSkillsExists($skillIds, $sourceType, $sourceId, $userSkillIds);
+
+        // prepare array to update
+        foreach ($data as $userSkill) {
+            $updateRecord[] = [
+                'id' => $userSkill['id'],
+                'source_type' => $sourceType,
+                'source_id' => $sourceId,
+                'skill_id' => $userSkill['skill_id']
+            ];
+        }
+
+        // update records
+        UserSkill::upsert($updateRecord, ['id'], ['skill_id']);
+
+        return true;
     }
 
     /**
@@ -123,17 +203,61 @@ class SkillService
     public function deleteUserSkill(int $userSkillId, int $userProfileId): bool
     {
         // get user skill model
-        $userSkill = UserSkill::find($userSkillId);
-
-        // check if this user skill belongs to this user
-        $this->verifySkillOwnership($userSkill, $userProfileId);
+        $userSkill = UserSkill::with('source')->find($userSkillId);
 
         if (!isset($userSkill)) {
             throw new Exception('User skill not found with given ID.', Response::HTTP_NOT_FOUND);
         }
 
+        // check if this user skill belongs to this user
+        $this->verifySkillOwnership($userSkill, $userProfileId);
+
         // delete user skill
         $result = $userSkill->delete();
+
+        return $result;
+    }
+
+    /**
+     * Deletes multiple existing user skills by user skill IDs
+     * (meant to be used by other services to delete their respective skills)
+     * 
+     * @param array $userSkillIds
+     * @param int $userProfileId
+     * @return bool
+     */
+    public function deleteUserSkillsByIds(array $userSkillIds, int $userProfileId): bool
+    {
+        if (empty($userSkillIds)) {
+            return true;
+        }
+
+        $userSkills = UserSkill::with('source')->whereIn('id', $userSkillIds)->get();
+
+        // verify if the user skills belong to the current user
+        $this->verifySkillOwnership($userSkills, $userProfileId);
+
+        $result = UserSkill::whereIn('id', $userSkillIds)->delete();
+
+        return $result;
+    }
+
+    /**
+     * Deletes multiple existing user skills by source type and source ID
+     * (meant to be used by other services to update their respective skills)
+     * 
+     * @param string $sourceType
+     * @param int $sourceId
+     * @return bool
+     */
+    public function deleteUserSkillsBySource(string $sourceType, int $sourceId): bool
+    {
+        // delete user skill
+        $result = UserSkill::where([
+            'source_type' => $sourceType,
+            'source_id' => $sourceId
+        ])
+            ->delete();
 
         return $result;
     }
